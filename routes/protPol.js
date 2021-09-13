43760f7f5210f0492e46bcc61a4394dbc3f5900b
Chris Huszcza <chris.huszcza@snyk.io>
2021-09-13 09:35:05 +0300

    feat: prototype pollution should work

--- routes/protPol.js
+++ routes/protPol.js
@@ -1,46 +1,19 @@
-function copyUsingForIn2(dst, src) {
-  for (let key in src) {
-    if (dst[key]) {
-      copyUsingForIn(dst[key], src[key]);
-    } else {
-      dst["a"] = key;
-      dst[key] = "a";
-      dst[key] // dcexpect NoEffectExpression
-      let obj ={}
-      obj[key] = "a"
-
-      if (key == "__proto__" || key == "constructor" || key == "prototype") continue;
-      obj[key] = key;
-    }
-  }
-}
-
-var objectProto = Object.prototype;
-var hasOwnProperty = objectProto.hasOwnProperty;
-
-function assignValue(object, key, value) {
-  var objValue = object[key];
-  if (!(hasOwnProperty.call(object, key) && eq(objValue, value)) ||
-    (value === undefined && !(key in object))) {
-    object[key] = value;
-  }
-}
-
-function assignValueLoop(keys) {
-  // keys is a PrototypePollutionFunctionParams source, but we check in assignValue
-  // that keys it is not __proto__.
-  let x = {};
-  for (let key in keys) {
-    assignValue(x, key, key);
-  }
-}
-
-var http = require('http'); // dcexpect HttpToHttps
-var fs = require('fs');
-
 var express = require('express');
-var obj = {};
-express().get('/', function(req, res) {
-  let obj = {};
-  obj[req.params.a][req.params.b] = req.params.c; // dcexpect TODO~PrototypePollution
+var app = express();
+app.use(express.json());
+
+
+/*
+http://localhost:3000/api?firstname=__proto__&nickname=snoopy
+*/
+
+obj = {};
+app.get('/api', function(req, res){
+  console.log(req.query.firstname);
+  result = obj[req.query.firstname] = req.query.nickname;
+  res.send(result);    // echo the result back
+
+
 });
+
+app.listen(3000);
